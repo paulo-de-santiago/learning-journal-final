7b2972d338e14ab9eebf7218bd02b07038550166
Paulo Santiago <pauloecsantiago@gmail.com>
2023-08-10 09:53:57 +0100

    Add files via upload
    Files upload

---
+++ home.js
@@ -0,0 +1,79 @@
+
+const articleComplete = document.getElementById("article article-opened")
+const articleButton = document.getElementById("article-listing-button-open")
+
+
+
+ articleButton.addEventListener("click", function(e)  {
+
+checkStatus()
+
+     
+})
+
+
+function checkStatus() {
+      
+   if(articleButton.id === "article-listing-button-open") {
+     articleButton.id = "article-listing-button-close" 
+     articleButton.innerText = 'View Less';
+     render()
+
+   } else if (articleButton.id === "article-listing-button-close") {
+     articleButton.id = "article-listing-button-open" 
+     articleButton.innerText = 'View More';
+     articleComplete.innerHTML = ''
+   }
+
+}
+
+  
+
+function render() {
+
+  
+  let articles = `<section class="article" id="article">
+            <article class="article-post" id="article-post">
+                <img src="/images/blog-image-02.png" alt="Computer screen with React symbol and code.">
+                <p class="article-date" id="article-date">JULY 23, 2022</p>
+                <h2 class="article-title" id="article-title">Blog one</h2>
+                <p class="article-body-preview" id="article-body-preview">I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.</p>   
+            </article>
+              <article class="article-post" id="article-post">
+                <img src="/images/blog-image-03.png" alt="Girl dressed casually on the beach in a sunny day.">
+                <p class="article-date" id="article-date">JULY 23, 2022</p>
+                <h2 class="article-title" id="article-title">Blog two</h2>
+                <p class="article-body-preview" id="article-body-preview">I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.</p>   
+            </article>
+            <article class="article-post" id="article-post">
+                <img src="/images/blog-image-04.png" alt="Laptop on wook desk with the message:&quot;I design and develop experiences that make people's lives simple.&quot;">
+                <p class="article-date" id="article-date">JULY 23, 2022</p>
+                <h2 class="article-title" id="article-title">Blog three</h2>
+                <p class="article-body-preview" id="article-body-preview">I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.</p>   
+            </article>
+            </section>
+             `  
+
+        articleComplete.innerHTML = articles 
+  
+}
+
+
+
+
+/* function seeMoreLess() {
+  let dots = document.getElementById("dots");
+  let moreText = document.getElementById("more");
+  let btnText = document.getElementById("see");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "block";
+    btnText.innerHTML = "View More";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "View Less";
+    moreText.style.display = "block";
+    moreText.style.flexWrap = "wrap"
+  }
+}   */
